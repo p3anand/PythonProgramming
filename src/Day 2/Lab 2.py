96750a0b0497ac2c130731008d987a8eb032d1a7
Preethi_Anand <p3anand.s@gmail.com>
2025-03-30 23:33:56 +0530

    Practice file - Lab 2 added

--- src/Day 2/Lab 2.py
+++ src/Day 2/Lab 2.py
@@ -1,12 +1,21 @@
 # Learning print function:
 # def print(self, *args, sep=' ', end='\n', file=None):  # known special case of print
 
-print ("Hello","This","is","a","trial",123,True,78.9)
+print("Hello", "This", "is", "a", "trial", 123, True, 78.9)
 
-#sep
-print ("Hello","This","is","a","trial",123,True,78.9,sep='-')
-print ("Hello","This","is","a","trial",123,True,78.9,sep='|')
+# sep
+print("Hello", "This", "is", "a", "trial", 123, True, 78.9, sep='-')
+print("Hello", "This", "is", "a", "trial", 123, True, 78.9, sep='|')
 
 # end="-"
-print("Hello",end="=")
+print("Hello", end="=")
 print("There")
+
+# Program to find the biggest number between integers using max function
+
+print(max(35, 100))
+
+print(max(35, 100, 300, 400))
+
+print(max(35, -1, -2, 400, 78.90, 45.90))
+
